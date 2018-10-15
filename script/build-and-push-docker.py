9f601409486f88f798dd58f2c20d087bdc0f2274
Eric Dattore <eric@dattore.me>
2018-10-15 16:24:24 -0600

    Add scaffolding for client app
    Working on getting a split frontend/backend app.

--- script/build-and-push-docker.py
+++ script/build-and-push-docker.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python
 
+import os
 import subprocess
 import toml
+import json
 
 
-def build_docker():
+def build_docker_server():
+    os.chdir("server")
     subprocess.call(
         [
             "docker",
             "build", "--rm",
             "-f", "docker/Dockerfile",
-            "-t", "edattore/rocket-app:v{0!s}".format(get_version()),
+            "-t", "edattore/rocket-app:v{0!s}".format(get_server_version()),
+            "."
+        ]
+    )
+
+
+def build_docker_client():
+    os.chdir("client")
+    subprocess.call(
+        [
+            "docker",
+            "build", "--rm",
+            "-f", "docker/Dockerfile",
+            "-t", "edattore/client-app:v{0!s}".format(get_client_version()),
             "."
         ]
     )
@@ -21,15 +37,30 @@ def push_docker():
         [
             "docker",
             "push",
-            "edattore/rocket-app:v{0!s}".format(get_version())
+            "edattore/rocket-app:v{0!s}".format(get_server_version())
+        ]
+    )
+
+    subprocess.call(
+        [
+            "docker",
+            "push",
+            "edattore/client-app:v{0!s}".format(get_client_version())
         ]
     )
 
 
-def get_version():
+def get_server_version():
     cargo_manifest = toml.load("Cargo.toml")
     return cargo_manifest["package"]["version"]
 
 
-build_docker()
+def get_client_version():
+    with open("package.json") as f:
+        node_manifest = json.load(f)
+        return node_manifest["version"]
+
+
+build_docker_server()
+build_docker_client()
 push_docker()
