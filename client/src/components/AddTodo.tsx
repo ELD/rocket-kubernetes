998124f6aeaba50f9210267b912213b012d60194
Eric Dattore <eric@dattore.me>
2018-10-16 10:13:29 -0600

    Starting point of todo app

---
+++ client/src/components/AddTodo.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+function AddTodo() {
+    return (
+        <h1>Add a todo!</h1>
+    );
+}
+
+export default AddTodo;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,23 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-import {EnthusiasmAction} from "./actions";
-import Hello from "./containers/Hello";
+import App from "./App";
 import './index.css';
-import {enthusiasm} from "./reducers";
 import registerServiceWorker from './registerServiceWorker';
-import {IStoreState} from "./types";
-
-const store = createStore<IStoreState, EnthusiasmAction, any, any>(enthusiasm, {
-    enthusiasmLevel: 1,
-    languageName: 'TypeScript',
-});
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Hello />
-    </Provider>,
+    <App />,
     document.getElementById('root') as HTMLElement
 );
 
