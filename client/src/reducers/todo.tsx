250556a721be56d9439ee671898a9570e9c1f9a6
Eric Dattore <eric.dattore@gmail.com>
2018-10-18 07:07:15 -0600

    Adding new todos work now

--- client/src/reducers/todo.tsx
+++ client/src/reducers/todo.tsx
@@ -1,28 +1,24 @@
 import {TodoAction} from "../actions";
 import {ADD_TODO, TOGGLE_TODO} from "../constants";
-import {DefaultState, IAppState, ITodo} from "../types/AppState";
+import {DefaultState, ITodo} from "../types/AppState";
 
-function todos(state: IAppState = DefaultState, action: TodoAction): IAppState {
+function todos(state: ITodo[] = DefaultState.todos, action: TodoAction): ITodo[] {
     console.log('app state: ', state);
     switch (action.type) {
         case ADD_TODO:
-            return {
-                todos: state.todos.push({
-                    completed: false,
-                    id: action.id,
-                    text: action.text,
-                }),
-                ...state
-            };
+            state.push({
+                completed: false,
+                id: action.id,
+                text: action.text
+            });
+            return state;
         case TOGGLE_TODO:
-            return {
-                todos: state.todos.map((todo: ITodo) => {
-                    return (todo.id === action.id)
-                        ? {...todo, completed: !todo.completed}
-                        : todo;
-                }),
-                ...state
-            };
+            state = state.map((todo: ITodo) => {
+                return (todo.id === action.id)
+                    ? {...todo, completed: !todo.completed}
+                    : todo;
+            });
+            return state;
         default:
             return state;
     }
